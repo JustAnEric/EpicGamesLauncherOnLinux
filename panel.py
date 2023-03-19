31dc760d9bd13059ab55df596515f1c1b27019cc
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-03-19 21:54:14 +1100

    Update panel.py

--- panel.py
+++ panel.py
@@ -2,7 +2,9 @@ import tkinter, webview
 from tkinter import Tk
 
 window = Tk()
+window.overrideredirect(True)
 
 label = tkinter.Label(window, text="Please wait while we edit the configuration in ./home/user/.cache/epicgames/make.properties").grid(column=0, row=0)
+btn = tkinter.Button(window, text="Cancel").grid(column=0, row=1)
 
 window.mainloop()
