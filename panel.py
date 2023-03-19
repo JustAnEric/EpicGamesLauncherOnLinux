11e8f7cd91982a3686841327c80746c38d3762b9
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-03-19 12:35:14 +1100

    Create panel.py

---
+++ panel.py
@@ -0,0 +1,9 @@
+import tkinter, pywebview
+from tkinter import Tk
+
+window = Tk()
+
+label = tkinter.Label(label="Please wait while we edit the configuration in ./home/user/.cache/epicgames/make.properties")
+label.pack(window)
+
+window.mainloop()
