1b84794a6c9cd1e7b12e37952f2d76769e30a7f5
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-03-19 22:06:20 +1100

    Update panel.py

--- panel.py
+++ panel.py
@@ -12,7 +12,8 @@ def close():
 label = tkinter.Label(window, text="Please wait while we edit the configuration in ./home/user/.cache/epicgames/make.properties").grid(column=0, row=0)
 btn = tkinter.Button(window, text="Cancel", command=close).grid(column=0, row=1)
 
-bar = ttk.Progressbar(window, length=200).grid(column=0, row=2)
+bar = ttk.Progressbar(window, length=200)
 bar['value'] = 70
+bar.grid(column=0, row=2)
 
 window.mainloop()
