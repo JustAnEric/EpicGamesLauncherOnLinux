ef004820d94476692202d278e3b51267a2be2749
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-03-19 21:37:12 +1100

    Update panel.py

--- panel.py
+++ panel.py
@@ -3,7 +3,7 @@ from tkinter import Tk
 
 window = Tk()
 
-label = tkinter.Label(label="Please wait while we edit the configuration in ./home/user/.cache/epicgames/make.properties")
+label = tkinter.Label(text="Please wait while we edit the configuration in ./home/user/.cache/epicgames/make.properties")
 label.pack(window)
 
 window.mainloop()
