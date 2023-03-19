95432e6806528e96942d5279d6b1325a47a94584
JustAnEric <73326807+JustAnEric@users.noreply.github.com>
2023-03-19 22:20:34 +1100

    Update panel.py

--- panel.py
+++ panel.py
@@ -5,7 +5,6 @@ window = Tk()
 window.overrideredirect(True)
 
 label = tkinter.Label(window, text="Please wait while we edit the configuration in ./home/user/.cache/epicgames/make.properties").grid(column=0, row=0)
-btn = tkinter.Button(window, text="Cancel", command=close).grid(column=0, row=1)
 
 bar = ttk.Progressbar(window, length=200)
 bar['value'] = 70
@@ -15,5 +14,7 @@ def close():
   res = messagebox.askyesno("Are you sure?", "Are you sure you want to close the application or process? This may affect the quality of your EpicGames installation.")
   if res: label.configure(text="Closing down..."); time.sleep(3); window.close()
   else: pass
+  
+btn = tkinter.Button(window, text="Cancel", command=close).grid(column=0, row=1)
 
 window.mainloop()
